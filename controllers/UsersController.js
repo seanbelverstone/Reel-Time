19177c636d271434fa8463a62091f2a991b0c8e4
Sean Belverstone <seanbelverstone@gmail.com>
2019-10-24 20:59:39 -0500

    Renamed "name" to "username" on the user model. Also created a "create" function on the controller and added the route for it. Will test on postman

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,5 +5,11 @@ module.exports = {
     db.User.findAll().then((users) => {
       response.json(users);
     });
+  },
+  create: (request, response) => {
+    db.User
+      .create(request.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => response.status(422).json(err));
   }
 };
--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
-    name: DataTypes.STRING,
+    username: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING
   }, {});
