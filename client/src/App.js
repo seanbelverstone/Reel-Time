dc6d15317994b5cbdf439b46ee2d5e6dd779a69f
Jihong Kim <jk279242naver.com>
2019-10-26 13:53:45 -0500

    deleted the app header from default react webpage.

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
 
         <div className="Login-field">
           <DefaultCard/>
