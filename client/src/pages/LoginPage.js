4b76c598cdd23e0c79f51edabfcb66b16761f8c3
Sean Belverstone <seanbelverstone@gmail.com>
2019-10-28 19:18:44 -0500

    Imported and added the BackgroundVideo into the login page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import DefaultCard from "../components/DefaultCard";
 import Logo from "../components/Logo";
 import LoginForm from "../components/LoginForm";
+import BackgroundVideo from "../components/BackgroundVideo";
 
 class LoginPage extends Component {
   render() {
 
     return (
         <div>
+            <BackgroundVideo />
             <DefaultCard>
             <Logo></Logo>
             <LoginForm></LoginForm>
