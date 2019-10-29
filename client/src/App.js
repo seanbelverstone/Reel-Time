0b47f5c0f37581c6180d7b09b2fef25d0c0985b4
Sean Belverstone <seanbelverstone@gmail.com>
2019-10-28 19:15:15 -0500

    Removed the background video link from App.js to hopefully prevent any conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,6 @@ class App extends Component {
     return (
       <div className="App">
 
-        <BackgroundVideo />
         <div className="Login-field">
           <DefaultCard/>
           {/* <Login/> */}
