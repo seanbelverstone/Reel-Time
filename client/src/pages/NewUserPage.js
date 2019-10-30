5222fcd2440af02a7e4d88c272c80d3ff710a43d
Jihong Kim <jk279242naver.com>
2019-10-30 18:44:18 -0500

    applied the same title mechanism to the New User Sign Up Page.

--- client/src/pages/NewUserPage.js
+++ client/src/pages/NewUserPage.js
@@ -3,14 +3,24 @@ import DefaultCard from "../components/DefaultCard";
 import Logo from "../components/Logo";
 import NewUserForm from "../components/NewUserForm";
 import BackgroundVideo from "../components/BackgroundVideo";
+import Title from "../components/TitleForEachPage";
+
 
 class NewUserPage extends Component {
+
+    state = {
+        title : "New User Sign Up"
+    }
+
     render() {
         return (
             <div>
             <BackgroundVideo />
             <DefaultCard>
-            <Logo />
+            <div className="page-header">
+                <Logo />
+                <Title title={this.state.title} />
+            </div>
             <NewUserForm />
             </DefaultCard>
             </div>
