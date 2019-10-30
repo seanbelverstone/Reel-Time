46b3500648269f5decdc1a0390ea608ae88d75e2
quyl <quylai@gmail.com>
2019-10-30 11:08:52 -0500

    translated server.js and mirror 'strategies' folder

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
+const dotEnv = require("dotenv");
+dotEnv.config();
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const LocalStrategy = require("./strategies/local");
+const JWTStrategy = require("./strategies/jwt");
 const routes = require("./routes");
 
-app.use(express.json())
-app.use(routes);
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+// app.use(logger('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+passport.use(LocalStrategy);
+passport.use(JWTStrategy);
  
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.use(routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
