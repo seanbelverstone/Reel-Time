d1a666123b49df00d1678d589bf4517c526bfbdb
Sean Belverstone <seanbelverstone@gmail.com>
2019-10-31 18:52:20 -0500

    Same again for the New Reel page

---
+++ client/src/components/NewReelCard/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class NewReelCard extends Component {
+  render() {
+    return (
+      <div className="newReelCard">
+        {this.props.children}
+        <span className="copyright">© 2019 Reel Time</span>
+      </div>
+    )
+  }
+}
+
+export default NewReelCard;
