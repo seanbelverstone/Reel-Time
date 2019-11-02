dd9713a01172da3ec7bb8ecd553c1ed1e46f9860
Jihong Kim <jk279242naver.com>
2019-11-01 21:37:26 -0500

    importing moment and timezone from npm.

--- client/src/components/SavedReelListItem/index.js
+++ client/src/components/SavedReelListItem/index.js
@@ -12,15 +12,13 @@ function SavedReelListItem (props) {
     return (
         <li>
             <div className="saved-content">
-                <div className="content-head">
-                    <p className="time-stamp">Watched: 00:00:00 / xx.xx.2019</p>
-                </div>
+                <p className="time-stamp">Watched: 00:00:00 / xx.xx.2019</p>
 
                 <img className="movie-image" src="https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg" alt=""></img>
 
                 <h5 className="movie-title">Title: Black Panther</h5>
 
-                <StarRating className="star-rating" />
+                <StarRating />
 
                 <p className="movie-synopsis"><span id="synopsis">Synopsis: </span>After the death of his father, T'Challa returns home to the African nation of Wakanda to take his rightful place as king. When a powerful enemy suddenly reappears, T'Challa's mettle as king -- and as Black Panther -- gets tested when he's drawn into a conflict that puts the fate of Wakanda and the entire world at risk. Faced with treachery and danger, the young king must rally his allies and release the full power of Black Panther to defeat his foes and secure the safety of his people.</p>
                 
