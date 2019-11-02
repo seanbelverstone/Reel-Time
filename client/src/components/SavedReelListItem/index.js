71a1d4c03754fed19b3aa3a7b8d77f096d17525c
Jihong Kim <jk279242naver.com>
2019-11-01 21:04:12 -0500

    added Star Rating Component.

--- client/src/components/SavedReelListItem/index.js
+++ client/src/components/SavedReelListItem/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Moment from "react-moment";
 import "moment-timezone";
 import "./style.css"
+import StarRating from "../StarRatingComponent";
 import ShareButton from "../ShareButton";
 import DeleteButton from "../DeleteButton";
 import ReplayButton from "../ReplayButton";
@@ -11,15 +12,24 @@ function SavedReelListItem (props) {
     return (
         <li>
             <div className="saved-content">
-                <p className="time-stamp">Watched: 00:00:00 / xx.xx.2019</p>
+                <div className="content-head">
+                    <p className="time-stamp">Watched: 00:00:00 / xx.xx.2019</p>
+                </div>
+
                 <img className="movie-image" src="https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg" alt=""></img>
+
                 <h5 className="movie-title">Title: Black Panther</h5>
+
+                <StarRating className="star-rating" />
+
                 <p className="movie-synopsis"><span id="synopsis">Synopsis: </span>After the death of his father, T'Challa returns home to the African nation of Wakanda to take his rightful place as king. When a powerful enemy suddenly reappears, T'Challa's mettle as king -- and as Black Panther -- gets tested when he's drawn into a conflict that puts the fate of Wakanda and the entire world at risk. Faced with treachery and danger, the young king must rally his allies and release the full power of Black Panther to defeat his foes and secure the safety of his people.</p>
+                
                 <ul className="buttons">
                     <li><ReplayButton /></li>
                     <li><ShareButton /></li>
                     <li><DeleteButton /></li>
                 </ul>
+
                 <div className="saved-recipe">
                     <p><span id="saved-recipe-head">Saved Recipe</span></p>
                     <p><span id="saved-recipe"># Menu: </span>Food Title Here</p>
