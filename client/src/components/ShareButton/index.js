023ce0c4469de15b0383979839f4df999b271873
Jihong Kim <jk279242naver.com>
2019-11-02 11:25:27 -0500

    downloaded npm for react-copy-to-clipboard. Share button will copy webpage link to clipboard. Worked on ShareButton Component.

--- client/src/components/ShareButton/index.js
+++ client/src/components/ShareButton/index.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import { Button } from "reactstrap";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import "./style.css";
 
 class ShareButton extends Component {
 
+    state = {
+        value: "",
+        copied: false,
+    };
+
     render () {
         return (
             <div>
-                <Button 
-                    a href="https://www.facebook.com/"
-                    target="_blank"
-                    onClick={this.onSubmit}
-                    id="share-button">SHARE</Button>
+                <CopyToClipboard 
+                    // Ryan: Whatever Webpage Link inside 'text' will be copied to clipboard, when User clicks Share Button. Currently using facebook link as an example, and it needs to be replaced with the App's link.
+                    text="https://www.facebook.com/"
+                    onCopy={() => this.setState({copied: true})}>
+                        <Button 
+                            id="share-button">SHARE
+                        </Button>
+                </CopyToClipboard>
+ 
+                {this.state.copied ? <span style={{color: 'blue'}}>Copied.</span> : null}
             </div>
         )
     }
