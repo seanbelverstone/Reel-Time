01f65f7588de5109c629f7445a2af5cc753fca96
Jihong Kim <jk279242naver.com>
2019-11-02 10:37:47 -0500

    share button is linked with facebook login page for now, and it will open up facebook on a new tab.

--- client/src/components/ShareButton/index.js
+++ client/src/components/ShareButton/index.js
@@ -8,6 +8,8 @@ class ShareButton extends Component {
         return (
             <div>
                 <Button 
+                    a href="https://www.facebook.com/"
+                    target="_blank"
                     onClick={this.onSubmit}
                     id="share-button">SHARE</Button>
             </div>
