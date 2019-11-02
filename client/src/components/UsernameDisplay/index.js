630b87da9fdc7cc074ebcf0e69a4c84a156a8281
quyl <quylai@gmail.com>
2019-11-02 18:04:43 -0500

    UsernameDisplay should now be display at the location it's placed

--- client/src/components/UsernameDisplay/index.js
+++ client/src/components/UsernameDisplay/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./style.css";
 
-function UsernameDisplay() {
-    return (
-        <p className="username-text">Hi, username!</p>
-    );
+const UsernameDisplay = () => {
+
+	// parsing username from document.cookie, document.cookie is defined in
+	// client\src\components\LoginForm\index.js
+	let temp = document.cookie.search("name=");
+	let uName = document.cookie.substring(temp+5, document.cookie.length);
+
+	return(
+		<div>
+			<p className="username-text">Hi, {uName}!</p>
+		</div>
+	);
 }
 
 export default UsernameDisplay;
