42393645a7eccd3eaae1f203e1e19aebfec63f16
Liz Guenthner <liz.guenthner@t-3.com>
2019-11-01 21:20:19 -0500

    fix username issue in cookies

--- client/src/pages/NewReelPage.js
+++ client/src/pages/NewReelPage.js
@@ -6,16 +6,11 @@ import NewReelListItem from "../components/NewReelListItem";
 import Title from "../components/TitleForEachPage";
 import UsernameDisplay from "../components/UsernameDisplay";
 
-const allCookies = document.cookie.split(";");
-const userNameToken = allCookies[14].split("=");
-const userNameValue = userNameToken[1];
-
 class NewReelPage extends Component {
     state = {
         movies: [],
         newReelMovieImg: "",
-        title: "New Reel",
-        username: userNameValue
+        title: "New Reel"
     }
     render () {
         return (
@@ -25,7 +20,7 @@ class NewReelPage extends Component {
                 <Logo />
                 <p className="title-div">
                     <Title title={this.state.title} />
-                    <UsernameDisplay username={this.state.username}/>
+                    <UsernameDisplay />
                 </p>
             </div>
             <div>
