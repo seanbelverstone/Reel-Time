7f04f850237fe51450a7fd0a3b74b4148f7131b4
Jihong Kim <jk279242naver.com>
2019-11-03 12:22:34 -0600

    divided the biggest div into two small divs: Movie-Data and Recipe-Data, and inserted mock images for demo for now.

--- client/src/components/NewReelListItem/index.js
+++ client/src/components/NewReelListItem/index.js
@@ -1,24 +1,39 @@
 import React, {Component} from "react";
 import "./style.css";
+import BackToDashButton from "../BackToDashButton";
 
 // BookListItem renders a list item containing data from the book api call
 class NewReelListItem extends Component {
-  state = {
-    newReelMovieImg: "asfkjd"
-  }
-  HandleSave = () => {
-    this.setState({text: "Saved"});
-    this.props.HandleClick();
-  }
+  // state = {
+  //   newReelMovieImg: BackToDashButton
+  // }
+
+  // HandleSave = () => {
+  //   this.setState({text: "Saved"});
+  //   this.props.HandleClick();
+  // }
 
   render() {
-    const {title, actors, link, description, newReelMovieImg} = this.props;
+    const {title, actors, link, description, newReelMovieImg, recipeTitle, recipeCuisine, recipeLink, newReelRecipeImg} = this.props;
+
   return (
     <li className="list-group-item">
-        <div className="movie-headers">
-            <p className="movie-title"><span>Title: </span>{title}</p>
-            <p className="movie-actors"><span>Actors: </span>{actors}</p>
-        </div>
+      <div className="movie-data">
+        <img className="movie-image" src="https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg" alt={title} />
+        {/* <img className="movie-image" src={newReelMovieImg} alt={title} /> */}
+        <p className="movie-title"><span>Title: </span>{title}</p>
+        <p className="movie-actors"><span>Actors: </span>{actors}</p>
+        <p className="movie-description"><span>Description: </span>{description}</p>
+      </div>
+
+      <div className="recipe-data">
+        <img className="recipe-image" src="https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2018/07/Cheap-family-meals-Recipes-under-%C2%A31-per-head-920x605.jpg" alt={recipeTitle} />
+        {/* <img className="recipe-image" src={newReelRecipeImg} alt={recipeTitle} /> */}
+        <p className="recipe-title"><span>Title: </span>{recipeTitle}</p>
+        <p className="recipe-cuisine"><span>Cuisine: </span>{recipeCuisine}</p>
+        <p className="recipe-link"><span>Link: </span>{recipeLink}</p>
+      </div>
+
         <div className="book-buttons">
             {/* {!isDelete ? (
                 need to pass this function name to the deconstructor
@@ -27,14 +42,9 @@ class NewReelListItem extends Component {
                 need to pass this function name to the deconstructor
             <button onClick={HandleClick}>Delete</button>
             )} */}
-            <a className="view-link btn" rel="noreferrer noopener" target="_blank" href={link}>
-            View
-            </a>
+            {/* <a className="view-link btn" rel="noreferrer noopener" target="_blank" href={link}>View</a> */}
         </div>
-        <div className="movie-data">
-            <img className="movie-image" src={newReelMovieImg} alt={title} />
-            <p className="movie-description"><span>Description: </span>{description}</p>
-        </div>
+
     </li>
   );
   }
