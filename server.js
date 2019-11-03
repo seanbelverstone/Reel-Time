087782a14eaba5805ad90e9cf351fe83c50e25a2
Sean Belverstone <seanbelverstone@gmail.com>
2019-11-02 20:42:58 -0500

    Added in an or clause for serving up the static assets on server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ passport.use(JWTStrategy);
 routes(app);
  
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV || process.env.JAWSDB_URL === "production") {
   app.use(express.static("client/build"));
 }
 
