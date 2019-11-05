ad52b66bdf580883901b3c54be10ebb122e1bd2f
Liz Guenthner <liz.guenthner@t-3.com>
2019-11-04 20:05:39 -0600

    fix username component

--- client/src/components/UsernameDisplay/index.js
+++ client/src/components/UsernameDisplay/index.js
@@ -6,7 +6,9 @@ const UsernameDisplay = () => {
 	// parsing username from document.cookie, document.cookie is defined in
 	// client\src\components\LoginForm\index.js
 	let temp = document.cookie.search("name=");
-	let uName = document.cookie.substring(temp+5, document.cookie.length);
+	let uNameArray = document.cookie.substring(temp+5).split(";");
+	let uName = uNameArray[0];
+	console.log(uName);
 
 	return(
 		<div>
