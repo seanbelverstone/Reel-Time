b2130fc7090214fdbba90c4a2a6805c573a8cb44
Sean Belverstone <seanbelverstone@gmail.com>
2019-11-04 22:44:19 -0600

    Pretty much done. Just need to clean up my code. Had to store the cuisine type and genre id to cookies, in order to access them again later to re-run the code. Added in a refresh part, so it refreshes the page every time a new one is loaded. If id used redux, could have just updated the state but ah well. You work with what cha get

--- client/src/pages/NewReelPage.js
+++ client/src/pages/NewReelPage.js
@@ -9,9 +9,12 @@ import BackToDashButton from "../components/BackToDashButton";
 import SaveAndWatchButton from "../components/SaveAndWatchButton";
 import StreamingService from "../components/StreamingService";
 import { Button } from "reactstrap";
+import API from "../utils/API";
 
 var movie;
 var recipe;
+var movieResults;
+var recipeResults;
 
 class NewReelPage extends Component {
     state = {
@@ -32,6 +35,57 @@ class NewReelPage extends Component {
         console.log(movie, recipe);
     }
 
+    reReel = () => {
+        this.movieSearch().then(() => {
+          this.recipeSearch().then(() => {
+            document.location.reload();
+          })
+        })
+    }
+
+    movieSearch = () => {
+        var movieCookie = document.cookie.split(";");
+        var movieSplit = movieCookie[3].split("=");
+        var movieValue = movieSplit[1];
+
+        return API.searchMovie(movieValue)
+          .then(results => {
+    
+            movieResults = results.data.results;
+    
+            // generates a random number
+            var randomNumber = Math.floor(Math.random() * 9) +1;
+    
+            // grabs a result in a random position, parses it into a string then adds it to local storage.
+            // Local storage can only accept strings
+            var movieString = JSON.stringify(movieResults[randomNumber]);
+            localStorage.setItem("movie", movieString);
+          });
+      }
+
+
+      recipeSearch = () => {
+        var cuisineCookie = document.cookie.split(";");
+        var cuisineSplit = cuisineCookie[4].split("=");
+        var cuisineValue = cuisineSplit[1];
+
+        console.log(cuisineValue);
+
+        return API.searchRecipe(cuisineValue)
+          .then(results => {
+    
+            recipeResults = results.data.hits;
+    
+            var randomNumber = Math.floor(Math.random() * 9) +1;
+    
+            // grabs a result in a random position, parses it into a string then adds it to local storage.
+            // Local storage can only accept strings
+            var recipeString = JSON.stringify(recipeResults[randomNumber]);
+            localStorage.setItem("recipe", recipeString);
+          });
+      }
+
+    
     render () {
         return (
             <div>
@@ -67,7 +121,7 @@ class NewReelPage extends Component {
 
             <div className="button-section">
                 <BackToDashButton/>
-                <Button />
+                <Button onClick={this.reReel}/>
                 <SaveAndWatchButton/>
             </div>
 
