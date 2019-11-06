49a1c55bfea1c9379a335051f48ee48d25fb2466
Sean Belverstone <seanbelverstone@gmail.com>
2019-11-06 15:21:53 -0600

    Build failed on Heroku as the imports were duplicated. Also added in the props.item section that was on a previous commit as a comment to test again later

--- client/src/components/SavedReelListItem/index.js
+++ client/src/components/SavedReelListItem/index.js
@@ -5,13 +5,6 @@ import ShareButton from "../ShareButton";
 import DeleteButton from "../DeleteButton";
 import ReplayButton from "../ReplayButton";
 
-import React from "react";
-import "./style.css"
-import StarRating from "../StarRatingComponent";
-import ShareButton from "../ShareButton";
-import DeleteButton from "../DeleteButton";
-import ReplayButton from "../ReplayButton";
-
 // RecipeList renders a bootstrap list item
 function SavedReelListItem (props) {
     return (
@@ -102,3 +95,44 @@ function SavedReelListItem (props) {
 }
 
 export default SavedReelListItem;
+
+
+// import React from "react";
+// import "./style.css"
+// import StarRating from "../StarRatingComponent";
+// import ShareButton from "../ShareButton";
+// import DeleteButton from "../DeleteButton";
+// import ReplayButton from "../ReplayButton";
+
+// // RecipeList renders a bootstrap list item
+// function SavedReelListItem (props) {
+//     return (
+//         <li>
+//             <div className="saved-content">
+//                 <p className="time-stamp">{`Watched: ${this.props.timestamp}`}</p>
+
+//                 <img className="movie-image" src={this.props.movieImage} alt=""></img>
+
+//                 <h5 className="movie-title">{this.props.movieTitle}</h5>
+
+//                 <StarRating />
+
+//                 <p className="movie-synopsis"><span id="synopsis">Synopsis: </span>{this.state.movieSynopsis}</p>
+                
+//                 <ul className="buttons">
+//                     <li><ReplayButton /></li>
+//                     <li><ShareButton /></li>
+//                     <li><DeleteButton /></li>
+//                 </ul>
+
+//                 <div className="saved-recipe">
+//                     <p><span id="saved-recipe-head">Saved Recipe</span></p>
+//                     <p><span id="saved-recipe"># Menu: </span>{this.props.recipeTitle}</p>
+//                     <p><span id="saved-recipe"># Link: </span><a href={this.props.recipeLink}>CLICKABLE LINK HERE</a></p>
+//                 </div>
+//             </div>
+//         </li>
+//     );
+// }
+
+// export default SavedReelListItem;
